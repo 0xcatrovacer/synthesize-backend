6bf765d5b4c1588cb354a934a4e72babc890417b
Swarnab Garang <swarnabgarang@gmail.com>
2021-05-11 20:49:22 +0530

    added track-router and minimal changes to track model

---
+++ src/routers/track-routers.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const Track = require('../models/track')
+
+const router = new express.Router()
+
+router.post('/track', async (req, res) => {
+    const track = new Track(req.body)
+
+    try {
+        await track.save()
+        res.status(201).send(track)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
+router.get('/tracks', async (req, res) => {
+    try {
+        const tracks = await Track.find();
+        res.status(200).send(tracks);
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
+module.exports = router
