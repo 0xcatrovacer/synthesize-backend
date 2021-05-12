28a02bd2fd1012af1ec342c2a1165c8a7ffd5a35
Swarnab Garang <swarnabgarang@gmail.com>
2021-05-12 19:46:40 +0530

    added user routers for google oauth

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('./db/mongoose')
 
 
+const userRouter = require('./routers/user-routers')
 const trackRouter = require('./routers/track-routers')
 
 
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 
+app.use(userRouter)
 app.use(trackRouter)
 
 
