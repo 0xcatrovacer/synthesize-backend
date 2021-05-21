bd03a363a96f3015681df3ffd7ebc2689cb43be0
Swarnab Garang <swarnabgarang@gmail.com>
2021-05-21 11:50:05 +0530

    many changes

--- src/routers/track-routers.js
+++ src/routers/track-routers.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const verify = require('../middlewares/verify')
 
 const Track = require('../models/Track')
+const getTracks = require('../utilities/searchAlgorithm.js')
 
-const router = new express.Router()
+const router = express.Router()
 
+
+// Post a new track to database
 router.post('/track', async (req, res) => {
     const track = new Track(req.body)
 
@@ -15,6 +19,7 @@ router.post('/track', async (req, res) => {
     }
 })
 
+// Get all tracks from database
 router.get('/tracks', async (req, res) => {
     try {
         const tracks = await Track.find();
@@ -23,5 +28,71 @@ router.get('/tracks', async (req, res) => {
         res.status(500).send(e.message)
     }
 })
+
+// Like a track
+// router.patch('/liketrack/:id', async (req, res) => {
+
+//     const updates = Object.keys(req.body)
+//     const allowedUpdates = ['likedby']
+//     const isValid = updates.every((update) => {
+//         return allowedUpdates.includes(update)
+//     })
+
+//     if (!isValid) {
+//         return res.status(400).send({ error: 'Invalid Update' })
+//     }
+
+//     try {
+
+//         const track = await Track.findOne({ _id: req.params.id, likedby: req.user._id })
+
+//         if (!track) {
+//             return res.status(404).send()
+//         }
+
+//         updates.forEach((update) => {
+//             track[update] = req.body[update]
+//         })
+
+//         await track.save()
+
+//         res.send(track)
+
+//     } catch (e) {
+//         res.status(500).send(e)
+//     }
+
+// })
+
+// //Show Liked Tracks
+// router.get('/likedtracks', async (req, res) => {
+//     try {
+//         const likedtracks = await req.user.populate({
+//             path: 'tracks'
+//         }).execPopulate()
+
+//         res.status(200).send(req.user.likedtracks)
+
+//     } catch (e) {
+
+//     }
+// })
+
+// router.post('/search', async (req, res) => {
+//     try {
+//         const searchInput = req.body.searchInput || ''
+//         const tracks = await Track.find()
+
+//         const result = getTracks(searchInput, tracks)
+
+//         const searchresults = [result[0], result[1], result[2], result[3], result[4]]
+
+//         res.status(200).send(searchresults)
+//     } catch (e) {
+//         res.status(500).send(e.message)
+//     }
+// })
+
+
 
 module.exports = router
